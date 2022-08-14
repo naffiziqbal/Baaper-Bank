cad34ba9a3523a68605c2377d8442f9a5a453ff9
naffiziqbal <f.nafiziqbal@gmail.com>
2022-08-15 00:57:41 +0600

    Hello

--- js/script.js
+++ js/script.js
@@ -1,42 +1,46 @@
 const userInfo = {
-    user : 'abc@gmail.com',
-    password : '342137',
+    user: 'abc@gmail.com',
+    password: '1234',
 };
 
 const userLog = {};
 const userNameFiled = document.getElementById('email');
 const password = document.getElementById('password');
 
-const userNameMatch =  userNameFiled.addEventListener('keyup', (e)=>{
+const userNameMatch = userNameFiled.addEventListener('keyup', (e) => {
 
     userLog.user = e.target.value;
-    if(e.target.value === userInfo.user){
+    if (e.target.value === userInfo.user) {
         console.log('Hello')
     }
-  
+
 });
-const passwordMatch = password.addEventListener('keyup', (e)=>{
+const passwordMatch = password.addEventListener('keyup', (e) => {
     userLog.password = e.target.value;
 
-    if(e.target.value === userInfo.password){
+    if (e.target.value === userInfo.password) {
         console.log('Jannati')
-    }else{
+    } else {
         console.log(`Jahannami Jahannami Jahannami`)
     }
 })
 
-document.getElementById('submit-btn').addEventListener('click', ()=>{
-    const wrongInfo = document.getElementById('worng-info-section')
-    if(userInfo.user === userLog.user && userInfo.password === userLog.password ){
+
+const page = document.getElementById('submit-btn');
+page.addEventListener('click', () => {
+    const wrongInfo = document.getElementById('worng-info-section');
+
+    if (userInfo.user === userLog.user && userInfo.password === userLog.password) {
+        window.location.href="./index.html";
         console.log("Free To Ride")
     }
-    else{
+    else {
         wrongInfo.classList.remove('d-none');
         wrongInfo.classList.add('d-block');
 
     }
 
-    
+
 })
 
 console.log(userLog)
